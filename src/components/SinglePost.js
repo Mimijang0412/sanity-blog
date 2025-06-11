e62cf182ac11c34a222296a8f626b00994250b6d
bluewingm <mimijang0412@gmail.com>
2025-06-11 16:27:24 -0400

    about page created

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import sanityClient from '../client.js';
+import sanityClient from '../client';
 import imageUrlBuilder from '@sanity/image-url';
 // import BlockContent from '@sanity/block-content-to-react';
 import { PortableText } from '@portabletext/react';
